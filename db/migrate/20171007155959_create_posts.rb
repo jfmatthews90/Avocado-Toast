88cb066ca40a7a36c8fa0e868cb291670f53eed8
James <jfmatthews90@gmail.com>
2017-10-07 12:04:56 -0400

    Created post scaffold.

---
+++ db/migrate/20171007155959_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.text :description
+      t.text :body
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
