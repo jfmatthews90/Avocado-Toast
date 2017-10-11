2b50ca70c56fbbf809bfd9a1d852332107c1966f
James <jfmatthews90@gmail.com>
2017-10-11 12:18:56 -0400

    Updated cancan ability to allow user to destroy and update only their own comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,14 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def destroy
+    @comment = Comment.find(params[:id])
+    post = @comment.post
+    @comment.destroy
+    redirect_to post
   end
 
   private
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+  user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+
+      can :read, :all
+      can :create, Comment
+      can [:update, :destroy], Comment, :user_id => user.id
+      can [:index, :show, :search], Post
+      cannot [:create, :edit], Post
+ 	  end
+
   end
 end
 
